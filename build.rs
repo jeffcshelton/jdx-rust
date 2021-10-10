b9e4cda26662e20d2beecf3d6aae2cdcad484fc2
jeffreycshelton <jeffrey.conner.shelton@gmail.com>
2021-10-10 02:32:52 -0400

    Use build path relative to manifest for use as dependency

--- build.rs
+++ build.rs
@@ -1,4 +1,9 @@
-use std::{env, io, process::Command};
+use std::{
+    env,
+    io,
+    path::Path,
+    process::Command
+};
 
 fn main() -> Result<(), io::ErrorKind> {
     env::set_current_dir("./libjdx")
@@ -15,6 +20,12 @@ fn main() -> Result<(), io::ErrorKind> {
         return Err(io::ErrorKind::Unsupported);
     }
 
-    println!("cargo:rustc-link-search=libjdx/lib");
+    let cargo_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let libjdx_link_dir = Path::new(&cargo_dir)
+        .join("libjdx")
+        .join("lib");
+
+    println!("cargo:rustc-link-search={}", libjdx_link_dir.display());
+
     Ok(())
 }
