54fe9fb44724fa7eaadfbfd475c3b03e806bc95e
Jeffrey Shelton <jeffrey.shelton.c@gmail.com>
2022-04-19 00:20:12 -0400

    Write read_header test Simplify read_dataset test

--- src/tests.rs
+++ src/tests.rs
@@ -3,12 +3,8 @@ use std::fs;
 
 #[test]
 fn read_dataset() -> Result<()> {
-	let dataset = Dataset::read_from_path("./libjdx-sys/libjdx/res/example.jdx")?;
-
-	assert_eq!(dataset.header.version, Version::current());
-	assert_eq!(dataset.header.image_count, 8);
-
-	return Ok(());
+	Dataset::read_from_path("./libjdx-sys/libjdx/res/example.jdx")
+		.map(|_| ())
 }
 
 #[test]
@@ -21,6 +17,19 @@ fn write_dataset() -> Result<()> {
 
 	fs::remove_file("./libjdx-sys/libjdx/res/temp.jdx")
 		.map_err(|_| Error::OpenFile("./libjdx-sys/libjdx/res/temp.jdx".to_owned()))?;
+
+	return Ok(());
+}
+
+#[test]
+fn read_header() -> Result<()> {
+	let header = Header::read_from_path("./libjdx-sys/libjdx/res/example.jdx")?;
+
+	assert_eq!(header.version, Version::current());
+	assert_eq!(header.bit_depth, 24);
+	assert_eq!(header.image_width, 52);
+	assert_eq!(header.image_height, 52);
+	assert_eq!(header.image_count, 8);
 
 	return Ok(());
 }
